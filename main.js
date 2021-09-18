60d2eb48616b0c7fd4e608e0483ed01b73162c75
Monir Uddin <monir74@gmail.com>
2021-09-18 15:49:24 +0600

    dashboad menu done

--- main.js
+++ main.js
@@ -2,3 +2,17 @@
 import './src/scss/main.scss'
 
 
+$(".accordion .accordion-head").click(function () {
+    if ($(this).next("div").is(":visible")) {
+        $(this).next("div").slideUp("normal");
+		$(this).find('i').removeClass('arrow-open').addClass('arrow-close');
+    } else if 
+        ($(this).next("div").is(":hidden")) {
+		$(this).next("div").slideDown("normal");
+		$(this).find('i').removeClass('arrow-close').addClass('arrow-open');
+    } else {
+        $(".accordion-body").slideUp("normal");
+        $(this).next("div").slideToggle("normal");
+		$(this).find('i').removeClass('arrow-open').addClass('arrow-close');
+    }
+});
