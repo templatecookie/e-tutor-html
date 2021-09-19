2f61b8420ce4817294d2fcb247342fdff26665fc
Monir Uddin <monir74@gmail.com>
2021-09-19 19:41:01 +0600

    features coures done

--- main.js
+++ main.js
@@ -19,12 +19,112 @@ $(".accordion .accordion-head").click(function () {
 
 
 
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+
+$(".custom-select").each(function() {
+  var classes = $(this).attr("class"),
+    id = $(this).attr("id"),
+    name = $(this).attr("name");
+  var template = '<div class="' + classes + '">';
+  template +=
+    '<span class="custom-select-trigger">' +
+    $(this).attr("placeholder") +
+    "</span>";
+  template += '<div class="custom-options">';
+  $(this)
+    .find("option")
+    .each(function() {
+      template +=
+        '<span class="custom-option ' +
+        $(this).attr("class") +
+        '" data-value="' +
+        $(this).attr("value") +
+        '">' +
+        $(this).html() +
+        "</span>";
+    });
+  template += "</div></div>";
+
+  $(this).wrap('<div class="custom-select-wrapper"></div>');
+  $(this).hide();
+  $(this).after(template);
+});
+$(".custom-option:first-of-type").hover(
+  function() {
+    $(this)
+      .parents(".custom-options")
+      .addClass("option-hover");
+  },
+  function() {
+    $(this)
+      .parents(".custom-options")
+      .removeClass("option-hover");
   }
-}
+);
+$(".custom-select-trigger").on("click", function() {
+  $("html").one("click", function() {
+    $(".custom-select").removeClass("opened");
+  });
+  $(this)
+    .parents(".custom-select")
+    .toggleClass("opened");
+  event.stopPropagation();
+});
+$(".custom-option").on("click", function() {
+  $(this)
+    .parents(".custom-select-wrapper")
+    .find("select")
+    .val($(this).data("value"));
+  $(this)
+    .parents(".custom-options")
+    .find(".custom-option")
+    .removeClass("selection");
+  $(this).addClass("selection");
+  $(this)
+    .parents(".custom-select")
+    .removeClass("opened");
+  $(this)
+    .parents(".custom-select")
+    .find(".custom-select-trigger")
+    .text($(this).text());
+});
+
+
+animateDisplay = function(target, animationClass, displayType, timeout) {
+  // timeout should be longer than css transition
+  var doneTimedDisplay = false,
+    displaying = false;
+
+  target.addEventListener('transitionend', function() {
+    if (!target.classList.contains('show')) {
+      target.style.display = 'none';
+    }
+    doneTimedDisplay = true;
+  });
+  if (!target.style.display || target.style.display === 'none') {
+    displaying = true;
+    target.style.display = displayType;
+  } else {
+    displaying = false;
+  }
+
+  setTimeout(function() {
+    target.classList.toggle(animationClass);
+    doneTimedDisplay = false;
+  }, 10);
+
+  if (!displaying) {
+    setTimeout(function() {
+      // failsafe for transitioned not firing
+      if (!doneTimedDisplay) {
+        target.style.display = 'none';
+      }
+      doneTimedDisplay = true;
+    }, timeout);
+  }
+};
+
+document.querySelector('.notttt')
+ .addEventListener('click', function() {
+ animateDisplay(document.querySelector('.heretheclass'), 'show', 'block', 600)
+});
 
