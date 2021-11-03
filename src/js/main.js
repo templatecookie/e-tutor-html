f55bd526111f2a1fb91fc89a02a21364f1ed6b5a
SheikhRashed <devrashed06@gmail.com>
2021-11-03 11:48:03 +0600

    removed cdn and added on local file of library js

--- src/js/main.js
+++ src/js/main.js
@@ -1,93 +1,67 @@
-// import main scss file
-import './src/scss/main.scss'
-
-
-$(".accordion .accordion-head").click(function () {
-    if ($(this).next("div").is(":visible")) {
-        $(this).next("div").slideUp("normal");
-		$(this).find('i').removeClass('arrow-open').addClass('arrow-close');
-    } else if 
-        ($(this).next("div").is(":hidden")) {
-		$(this).next("div").slideDown("normal");
-		$(this).find('i').removeClass('arrow-close').addClass('arrow-open');
-    } else {
-        $(".accordion-body").slideUp("normal");
-        $(this).next("div").slideToggle("normal");
-		$(this).find('i').removeClass('arrow-open').addClass('arrow-close');
-    }
+$('.accordion .accordion-head').click(function () {
+  if ($(this).next('div').is(':visible')) {
+    $(this).next('div').slideUp('normal');
+    $(this).find('i').removeClass('arrow-open').addClass('arrow-close');
+  } else if ($(this).next('div').is(':hidden')) {
+    $(this).next('div').slideDown('normal');
+    $(this).find('i').removeClass('arrow-close').addClass('arrow-open');
+  } else {
+    $('.accordion-body').slideUp('normal');
+    $(this).next('div').slideToggle('normal');
+    $(this).find('i').removeClass('arrow-open').addClass('arrow-close');
+  }
 });
-
 
 //this month form
 
-$(".custom-select").each(function() {
-  var classes = $(this).attr("class"),
-    id = $(this).attr("id"),
-    name = $(this).attr("name");
+$('.custom-select').each(function () {
+  var classes = $(this).attr('class'),
+    id = $(this).attr('id'),
+    name = $(this).attr('name');
   var template = '<div class="' + classes + '">';
   template +=
-    '<span class="custom-select-trigger">' +
-    $(this).attr("placeholder") +
-    "</span>";
+    '<span class="custom-select-trigger">' + $(this).attr('placeholder') + '</span>';
   template += '<div class="custom-options">';
   $(this)
-    .find("option")
-    .each(function() {
+    .find('option')
+    .each(function () {
       template +=
         '<span class="custom-option ' +
-        $(this).attr("class") +
+        $(this).attr('class') +
         '" data-value="' +
-        $(this).attr("value") +
+        $(this).attr('value') +
         '">' +
         $(this).html() +
-        "</span>";
+        '</span>';
     });
-  template += "</div></div>";
+  template += '</div></div>';
 
   $(this).wrap('<div class="custom-select-wrapper"></div>');
   $(this).hide();
   $(this).after(template);
 });
-$(".custom-option:first-of-type").hover(
-  function() {
-    $(this)
-      .parents(".custom-options")
-      .addClass("option-hover");
+$('.custom-option:first-of-type').hover(
+  function () {
+    $(this).parents('.custom-options').addClass('option-hover');
   },
-  function() {
-    $(this)
-      .parents(".custom-options")
-      .removeClass("option-hover");
-  }
+  function () {
+    $(this).parents('.custom-options').removeClass('option-hover');
+  },
 );
-$(".custom-select-trigger").on("click", function() {
-  $("html").one("click", function() {
-    $(".custom-select").removeClass("opened");
+$('.custom-select-trigger').on('click', function () {
+  $('html').one('click', function () {
+    $('.custom-select').removeClass('opened');
   });
-  $(this)
-    .parents(".custom-select")
-    .toggleClass("opened");
+  $(this).parents('.custom-select').toggleClass('opened');
   event.stopPropagation();
 });
-$(".custom-option").on("click", function() {
-  $(this)
-    .parents(".custom-select-wrapper")
-    .find("select")
-    .val($(this).data("value"));
-  $(this)
-    .parents(".custom-options")
-    .find(".custom-option")
-    .removeClass("selection");
-  $(this).addClass("selection");
-  $(this)
-    .parents(".custom-select")
-    .removeClass("opened");
-  $(this)
-    .parents(".custom-select")
-    .find(".custom-select-trigger")
-    .text($(this).text());
+$('.custom-option').on('click', function () {
+  $(this).parents('.custom-select-wrapper').find('select').val($(this).data('value'));
+  $(this).parents('.custom-options').find('.custom-option').removeClass('selection');
+  $(this).addClass('selection');
+  $(this).parents('.custom-select').removeClass('opened');
+  $(this).parents('.custom-select').find('.custom-select-trigger').text($(this).text());
 });
-
 
 // animateDisplay = function(target, animationClass, displayType, timeout) {
 //   // timeout should be longer than css transition
@@ -123,78 +97,71 @@ $(".custom-option").on("click", function() {
 //   }
 // };
 
-
-
-
 // range slider
 
-var inputLeft = document.getElementById("input-left");
-var inputRight = document.getElementById("input-right");
+var inputLeft = document.getElementById('input-left');
+var inputRight = document.getElementById('input-right');
 
-var thumbLeft = document.querySelector(".slider > .thumb.left");
-var thumbRight = document.querySelector(".slider > .thumb.right");
-var range = document.querySelector(".slider > .range");
+var thumbLeft = document.querySelector('.slider > .thumb.left');
+var thumbRight = document.querySelector('.slider > .thumb.right');
+var range = document.querySelector('.slider > .range');
 
 function setLeftValue() {
-	var _this = inputLeft,
-		min = parseInt(_this.min),
-		max = parseInt(_this.max);
+  var _this = inputLeft,
+    min = parseInt(_this.min),
+    max = parseInt(_this.max);
 
-	_this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
+  _this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
 
-	var percent = ((_this.value - min) / (max - min)) * 100;
+  var percent = ((_this.value - min) / (max - min)) * 100;
 
-	thumbLeft.style.left = percent + "%";
-	range.style.left = percent + "%";
+  thumbLeft.style.left = percent + '%';
+  range.style.left = percent + '%';
 }
 setLeftValue();
 
 function setRightValue() {
-	var _this = inputRight,
-		min = parseInt(_this.min),
-		max = parseInt(_this.max);
+  var _this = inputRight,
+    min = parseInt(_this.min),
+    max = parseInt(_this.max);
 
-	_this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
+  _this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
 
-	var percent = ((_this.value - min) / (max - min)) * 100;
+  var percent = ((_this.value - min) / (max - min)) * 100;
 
-	thumbRight.style.right = (100 - percent) + "%";
-	range.style.right = (100 - percent) + "%";
+  thumbRight.style.right = 100 - percent + '%';
+  range.style.right = 100 - percent + '%';
 }
 setRightValue();
 
-inputLeft.addEventListener("input", setLeftValue);
-inputRight.addEventListener("input", setRightValue);
+inputLeft.addEventListener('input', setLeftValue);
+inputRight.addEventListener('input', setRightValue);
 
-inputLeft.addEventListener("mouseover", function() {
-	thumbLeft.classList.add("hover");
+inputLeft.addEventListener('mouseover', function () {
+  thumbLeft.classList.add('hover');
 });
-inputLeft.addEventListener("mouseout", function() {
-	thumbLeft.classList.remove("hover");
+inputLeft.addEventListener('mouseout', function () {
+  thumbLeft.classList.remove('hover');
 });
-inputLeft.addEventListener("mousedown", function() {
-	thumbLeft.classList.add("active");
+inputLeft.addEventListener('mousedown', function () {
+  thumbLeft.classList.add('active');
 });
-inputLeft.addEventListener("mouseup", function() {
-	thumbLeft.classList.remove("active");
-});
-
-inputRight.addEventListener("mouseover", function() {
-	thumbRight.classList.add("hover");
-});
-inputRight.addEventListener("mouseout", function() {
-	thumbRight.classList.remove("hover");
-});
-inputRight.addEventListener("mousedown", function() {
-	thumbRight.classList.add("active");
-});
-inputRight.addEventListener("mouseup", function() {
-	thumbRight.classList.remove("active");
+inputLeft.addEventListener('mouseup', function () {
+  thumbLeft.classList.remove('active');
 });
 
-
-
-
+inputRight.addEventListener('mouseover', function () {
+  thumbRight.classList.add('hover');
+});
+inputRight.addEventListener('mouseout', function () {
+  thumbRight.classList.remove('hover');
+});
+inputRight.addEventListener('mousedown', function () {
+  thumbRight.classList.add('active');
+});
+inputRight.addEventListener('mouseup', function () {
+  thumbRight.classList.remove('active');
+});
 
 //chart.js
 
@@ -217,7 +184,7 @@ var areachartUserOption_2s = {
   stroke: { curve: 'smooth', width: 3 },
   series: [
     {
-      data: [100, 99, 98, 90,100, 90, 100, 90,100, 90, 100, 90],
+      data: [100, 99, 98, 90, 100, 90, 100, 90, 100, 90, 100, 90],
     },
   ],
   colors: ['#23BD33'],
@@ -253,11 +220,8 @@ var areachartUserOption_2s = {
 if (document.getElementById('area-chart-user2')) {
   var chart_area_2 = new ApexCharts(
     document.querySelector('#area-chart-user2'),
-    areachartUserOption_2s
+    areachartUserOption_2s,
   );
 
   chart_area_2.render();
 }
-
-
-
