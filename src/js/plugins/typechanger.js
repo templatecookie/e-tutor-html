4df7f926f0ffb6757a5f8ec55b888f0e2902aa31
SheikhRashed <devrashed06@gmail.com>
2021-11-29 19:10:34 +0600

    typechange img path fixed

--- src/js/plugins/typechanger.js
+++ src/js/plugins/typechanger.js
@@ -3,9 +3,9 @@ function showPassword(id, el) {
   let x = document.getElementById(id);
   if (x.type === 'password') {
     x.type = 'text';
-    el.innerHTML = `<img src="src/images/svg-icon/eye-close.svg" alt="eye-icon">`;
+    el.innerHTML = `<img src="assets/images/svg-icon/eye-close.svg" alt="eye-icon">`;
   } else {
     x.type = 'password';
-    el.innerHTML = `<img src="src/images/svg-icon/eye-open.svg" alt="eye-icon">`;
+    el.innerHTML = `<img src="assets/images/svg-icon/eye-open.svg" alt="eye-icon">`;
   }
 }
